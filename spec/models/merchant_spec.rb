d0671a1479032747c3ab433e1c3f2e68224abfbd
Austin Moore <austinchristianmoore@gmail.com>
2022-03-29 11:24:44 -0400

    Feature: Create successful response

---
+++ spec/models/merchant_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Merchant, type: :model do
+  # has_many :items
+  # has_many :invoice_items, through: :items
+  # has_many :invoices, through: :invoice_items
+  # has_many :customers, through: :invoices
+  # has_many :transactions, through: :invoices
+
+  # validates_presence_of(:name)
+  # validates_presence_of(:status)
+  # enum status: {"disabled" => 0, "enabled" => 1}
+end
