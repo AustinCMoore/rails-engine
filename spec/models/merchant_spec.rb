4180b36e5ba590a3228a06aa6bd6e95c905796ef
Austin Moore <austinchristianmoore@gmail.com>
2022-03-29 11:34:28 -0400

    Feature: Complete merchants list test

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,13 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
-  # has_many :items
-  # has_many :invoice_items, through: :items
-  # has_many :invoices, through: :invoice_items
-  # has_many :customers, through: :invoices
-  # has_many :transactions, through: :invoices
 
-  # validates_presence_of(:name)
-  # validates_presence_of(:status)
-  # enum status: {"disabled" => 0, "enabled" => 1}
 end
