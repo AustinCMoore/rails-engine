50c32657c09bf9ba6f605cd681d8c7a7f4b74e4e
Austin Moore <austinchristianmoore@gmail.com>
2022-03-30 19:39:36 -0400

    Feature: Create sad path for merchant items

--- app/controllers/api/v1/merchant_items_controller.rb
+++ app/controllers/api/v1/merchant_items_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::MerchantItemsController < ApplicationController
   def index
-    render json: ItemSerializer.new(Merchant.find(params[:merchant_id]).items)
+    if Merchant.exists?(params[:merchant_id])
+      render json: ItemSerializer.new(Merchant.find(params[:merchant_id]).items)
+    else
+      render :status => 404
+    end
   end
 end
--- spec/requests/api/v1/merchant_items_request_spec.rb
+++ spec/requests/api/v1/merchant_items_request_spec.rb
@@ -17,23 +17,22 @@ RSpec.describe "Merchant Items API" do
 
     expect(items[:data].count).to eq(2)
 
-      items[:data].each do |item|
-        expect(item).to have_key(:id)
-        expect(item[:id]).to be_an(String)
+    items[:data].each do |item|
+      expect(item).to have_key(:id)
+      expect(item[:id]).to be_an(String)
 
-        expect(item[:attributes]).to have_key(:name)
-        expect(item[:attributes][:name]).to be_an(String)
+      expect(item[:attributes]).to have_key(:name)
+      expect(item[:attributes][:name]).to be_an(String)
 
-        expect(item[:attributes]).to have_key(:description)
-        expect(item[:attributes][:description]).to be_an(String)
+      expect(item[:attributes]).to have_key(:description)
+      expect(item[:attributes][:description]).to be_an(String)
 
-        expect(item[:attributes]).to have_key(:unit_price)
-        expect(item[:attributes][:unit_price]).to be_an(Float)
+      expect(item[:attributes]).to have_key(:unit_price)
+      expect(item[:attributes][:unit_price]).to be_an(Float)
 
-        expect(item[:attributes]).to have_key(:merchant_id)
-        expect(item[:attributes][:merchant_id]).to be_an(Integer)
-        expect(item[:attributes][:merchant_id]).to eq(merchant_1.id)
-      end
-
+      expect(item[:attributes]).to have_key(:merchant_id)
+      expect(item[:attributes][:merchant_id]).to be_an(Integer)
+      expect(item[:attributes][:merchant_id]).to eq(merchant_1.id)
+    end
   end
 end
