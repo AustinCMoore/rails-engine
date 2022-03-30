591f617a8b4b9963a6e94aee3a8f726c674daca7
Austin Moore <austinchristianmoore@gmail.com>
2022-03-30 18:00:37 -0400

    Feature: Add sad path for merchant show

--- app/controllers/api/v1/item_merchant_controller.rb
+++ app/controllers/api/v1/item_merchant_controller.rb
@@ -1,8 +1,6 @@
 class Api::V1::ItemMerchantController < ApplicationController
   def index
-    # binding.pry
     item = Item.find(params[:item_id])
-    # binding.pry
     render json: MerchantSerializer.new(item.merchant)
   end
 end
