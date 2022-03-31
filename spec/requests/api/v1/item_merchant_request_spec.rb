f90577a5b510ab986bde76be16623eb39bbd6bce
Austin Moore <austinchristianmoore@gmail.com>
2022-03-31 10:32:03 -0400

    Feature: Create sad paths for item merchant

--- spec/requests/api/v1/item_merchant_request_spec.rb
+++ spec/requests/api/v1/item_merchant_request_spec.rb
@@ -12,8 +12,28 @@ RSpec.describe "Item's Merchant API" do
     expect(response).to be_successful
 
     merchant = JSON.parse(response.body, symbolize_names: true)
-    
+
     expect(merchant[:data][:id]).to eq(merchant_1.id.to_s)
     expect(merchant[:data][:id]).to_not eq(merchant_2.id.to_s)
+  end
+
+  it "returns 404 with a bad id" do
+    merchant_1 = create(:merchant)
+    item_1 = merchant_1.items.create!(name: Faker::Esport.game, description: Faker::Esport.event, unit_price: Faker::Number.decimal)
+    id = item_1.id + 1
+
+    get "/api/v1/items/#{id}/merchant"
+
+    expect(response.status).to eq(404)
+  end
+
+  it "returns 404 with a string id" do
+    merchant_1 = create(:merchant)
+    item_1 = merchant_1.items.create!(name: Faker::Esport.game, description: Faker::Esport.event, unit_price: Faker::Number.decimal)
+    id = "eleven"
+
+    get "/api/v1/items/#{id}/merchant"
+
+    expect(response.status).to eq(404)
   end
 end
