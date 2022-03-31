00f33de5233edff4e0101dd54fdc35441275aeb2
Austin Moore <austinchristianmoore@gmail.com>
2022-03-31 16:51:04 -0400

    Feature: Add model methods

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -11,4 +11,42 @@ RSpec.describe Item, type: :model do
     it { should validate_presence_of(:unit_price) }
     it { should validate_numericality_of(:unit_price) }
   end
+
+  describe 'class methods' do
+    it "#search_by_name" do
+      merchant = Merchant.create!(name: "Austin")
+      item_1 = merchant.items.create(name: "Ring", description: "This is jewlery", unit_price: 1000.00)
+      item_2 = merchant.items.create(name: "Cat Toy", description: "This is for cats", unit_price: 0.99)
+      item_3 = merchant.items.create(name: "Cat Food", description: "This is for cats", unit_price: 9.99)
+
+      expect(Item.search_by_name("Cat")).to eq([item_3, item_2])
+    end
+
+    it "#search_by_min_price" do
+      merchant = Merchant.create!(name: "Austin")
+      item_1 = merchant.items.create(name: "Ring", description: "This is jewlery", unit_price: 1000.00)
+      item_2 = merchant.items.create(name: "Cat Toy", description: "This is for cats", unit_price: 0.99)
+      item_3 = merchant.items.create(name: "Cat Food", description: "This is for cats", unit_price: 9.99)
+
+      expect(Item.search_by_min_price("1.00")).to eq([item_3, item_1])
+    end
+
+    it "#search_by_max_price" do
+      merchant = Merchant.create!(name: "Austin")
+      item_1 = merchant.items.create(name: "Ring", description: "This is jewlery", unit_price: 1000.00)
+      item_2 = merchant.items.create(name: "Cat Toy", description: "This is for cats", unit_price: 0.99)
+      item_3 = merchant.items.create(name: "Cat Food", description: "This is for cats", unit_price: 9.99)
+
+      expect(Item.search_by_max_price("999.99")).to eq([item_3, item_2])
+    end
+
+    it "#search_by_price_range" do
+      merchant = Merchant.create!(name: "Austin")
+      item_1 = merchant.items.create(name: "Ring", description: "This is jewlery", unit_price: 1000.00)
+      item_2 = merchant.items.create(name: "Cat Toy", description: "This is for cats", unit_price: 0.99)
+      item_3 = merchant.items.create(name: "Cat Food", description: "This is for cats", unit_price: 9.99)
+
+      expect(Item.search_by_price_range("1.00", "999.99")).to eq([item_3])
+    end
+  end
 end
